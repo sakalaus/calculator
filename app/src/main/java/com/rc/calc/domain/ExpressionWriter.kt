0456f9f4cf07c6752cd6f94ec585fa39ac068923
sakalaus@gmai.com <sakalaus@gmail.com>
2022-11-26 15:57:33 +0200

    Expression writer

---
+++ app/src/main/java/com/rc/calc/domain/ExpressionWriter.kt
@@ -0,0 +1,88 @@
+package com.rc.calc.domain
+
+import ExpressionEvaluator
+
+class ExpressionWriter {
+
+    var expression = ""
+
+    fun processAction(action: CalculatorAction) {
+        when(action) {
+            CalculatorAction.Calculate -> {
+                val parser = ExpressionParser(prepareForCalculation())
+                val evaluator = ExpressionEvaluator(parser.parse())
+                expression = evaluator.evaluate().toString()
+            }
+            CalculatorAction.Clear -> {
+                expression = ""
+            }
+            CalculatorAction.Decimal -> {
+                if(canEnterDecimal()) {
+                    expression += "."
+                }
+            }
+            CalculatorAction.Delete -> {
+                expression = expression.dropLast(1)
+            }
+            is CalculatorAction.Number -> {
+                expression += action.number
+            }
+            is CalculatorAction.Op -> {
+                if(canEnterOperation(action.operation)) {
+                    expression += action.operation.symbol
+                }
+            }
+            CalculatorAction.Parentheses -> {
+                processParentheses()
+            }
+        }
+    }
+
+    private fun prepareForCalculation(): String {
+        val newExpression = expression.takeLastWhile {
+            it in "$operationSymbols(."
+        }
+        if(newExpression.isEmpty()) {
+            return "0"
+        }
+        return newExpression
+    }
+
+    private fun processParentheses() {
+        val openingCount = expression.count { it == '(' }
+        val closingCount = expression.count { it == ')' }
+        expression += when {
+            expression.isEmpty() ||
+                    expression.last() in "$operationSymbols(" -> "("
+            expression.last() in "0123456789)" &&
+                    openingCount == closingCount -> return
+            else -> ")"
+        }
+    }
+
+    private fun canEnterDecimal(): Boolean {
+        if(expression.isEmpty() || expression.last() in "$operationSymbols.()") {
+            return false
+        }
+        return !expression.takeLastWhile {
+            it in "0123456789."
+        }.contains(".")
+    }
+
+    private fun canEnterOperation(operation: Operation): Boolean {
+        if(operation in listOf(Operation.ADD, Operation.SUBTRACT)) {
+            return expression.isEmpty() || expression.last() in "$operationSymbols()0123456789"
+        }
+        return expression.isNotEmpty() || expression.last() in "0123456789)"
+    }
+}
+
+sealed interface CalculatorAction {
+    data class Number(val number: Int): CalculatorAction
+    data class Op(val operation: Operation): CalculatorAction
+    object Clear: CalculatorAction
+    object Delete: CalculatorAction
+    object Parentheses: CalculatorAction
+    object Calculate: CalculatorAction
+    object Decimal: CalculatorAction
+}
