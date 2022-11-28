d1ce75cbdbfcb1df2ea0813144c9598c30234928
sakalaus <sakalaus@gmail.com>
2022-11-28 10:23:46 +0200

    Ui with a test

--- app/src/main/java/com/rc/calc/MainActivity.kt
+++ app/src/main/java/com/rc/calc/MainActivity.kt
@@ -3,40 +3,27 @@ package com.rc.calc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.rc.calc.ui.theme.CalculatorTheme
+import com.rc.calc.presentation.CalculatorScreen
+import com.rc.calc.ui.theme.MaterialCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Greeting("Android")
-                }
+            MaterialCalculatorTheme {
+                CalculatorScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CalculatorTheme {
-        Greeting("Android")
+    MaterialCalculatorTheme {
+        CalculatorScreen()
     }
 }
